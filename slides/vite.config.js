e9b77a37422c9fb9eee77b0639e226e1010f536f
Orlando Fornolles Jr <orlandojr058@gmail.com>
2026-07-30 23:56:32 +0800

    feat: configure app.html as Vite input entrypoint and push compiled slides with new OG image

--- slides/vite.config.js
+++ slides/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,6 +8,11 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'app.html')
+      }
+    }
   },
   base: './'
 })
